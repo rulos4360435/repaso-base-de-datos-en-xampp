9e8e761cc8fe5e2c33b3978a0bd74691d5096571
john <jhonandres4360435@gmail.com>
2025-08-15 16:07:32 -0500

    creando las rutas y los modelos

---
+++ app/Models/hospital.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class hospital extends Model
+{
+    //
+    protected $table ="hospital";
+    protected $fillable =["sede","ciudad"];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\hospital;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +10,11 @@ Route::get('/', function () {
 
 Route::get("/mision",function(){
     return view("mision");
+});
+Route::get("crear-hospital",function(){
+    $hospital = new hospital();
+    $hospital->sede="norte";
+    $hospital->ciudad="cali";
+    $hospital->save();
+    return "datos registrados correctamente";
 });
